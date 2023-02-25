c497e60531f492e4a7261e2dd5361d1ca8474c94
hoonkim <hoonk9387@gmail.com>
2023-02-24 21:02:08 -0600

    In main created two routes '/' and 'about' for GET request. Created responses for GET request.

---
+++ main.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+// Create route '/'
+// Create GET request response
+app.get('/', (req, res) => {
+    res.send("Welcome to the home page.")
+})
+
+// Create route '/'
+// Create GET request response
+app.get('/about', (req, res) => {
+    res.send("Thanks for learning more about us.")
+})
+
+
+
+app.listen(3000)
