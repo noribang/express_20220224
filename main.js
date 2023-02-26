1942314942bc7f0ed52f63bca2828d67c4f2339c
hoonkim <hoonk9387@gmail.com>
2023-02-26 12:27:53 -0600

    Get request to route '/'. Created response.

--- main.js
+++ main.js
@@ -2,3 +2,14 @@
 const express = require('express')
 // Create new instance of express.
 const app = express()
+
+// Route '/'.
+// Get request.
+app.get("/", (req, res) => {
+    res.send("Welcome to our home page.")
+})
+
+
+
+// Server listen at port 4000.
+app.listen(3000)
