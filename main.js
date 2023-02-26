9ced2d88863b09ef383464931acd3088b617dd37
hoonkim <hoonk9387@gmail.com>
2023-02-26 12:49:57 -0600

    Refactored get request to route '/' to repond with form.

--- main.js
+++ main.js
@@ -3,11 +3,24 @@ const express = require('express')
 // Create new instance of express.
 const app = express()
 
-// Route '/'.
-// Get request.
-app.get("/", (req, res) => {
-    res.send("Welcome to our home page.")
+/// Route '/'.
+/// Get request.
+// app.get("/", (req, res) => {
+//     res.send("Welcome to our home page.")
+// })
+app.get('/', (req, res) => {
+    res.send(`
+        <h1>What color is the sky on a clear day?</h1>
+        <form action="/result" method="POST">
+            <input type="text" name="color">
+            <button>Submit Answer</button>
+        </form>
+    `)
 })
+
+
+
+
 // Route '/about'
 // Get request.
 app.get('/about', (req, res) => {
