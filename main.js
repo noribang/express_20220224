4f2bba745fe38c0a1d1b631b25fad0734ace9872
hoonkim <hoonk9387@gmail.com>
2023-02-26 12:18:32 -0600

    Required express package.

--- main.js
+++ main.js
@@ -1,18 +1,2 @@
+// Require express package.
 const express = require('express')
-const app = express()
-
-// Create route '/'.
-// Create GET request response for route '/'.
-app.get('/', (req, res) => {
-    res.send("Welcome to the home page.")
-})
-
-// Create route '/'
-// Create GET request response
-app.get('/about', (req, res) => {
-    res.send("Thanks for learning more about us.")
-})
-
-
-
-app.listen(3000)
